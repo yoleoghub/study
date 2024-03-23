d68c0ba28e4fb673d36e1ab855ed3a0897eef3f6
yoleoghub <164607154+yoleoghub@users.noreply.github.com>
2024-03-23 15:18:59 +0900

    Add files via upload

---
+++ java/HelloJava.java
@@ -0,0 +1,13 @@
+public class HelloJava {
+
+    public static void main(String[] args) {    // [단축어] psvm: main() 자동 완성
+        System.out.println("hello java1");      // [단축어] sout: System.out.println() 자동 완성
+        System.out.println("hello java2");      // [주석comment] hello java2를 출력합니다.(한 줄 주석 single line comment)
+        //System.out.println("hello java3");
+
+        /* [여러 줄 주석multi line comment]
+        System.out.println("hello java2");
+        System.out.println("hello java2");
+         */
+    }
+}
