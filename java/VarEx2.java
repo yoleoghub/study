27a1ed74cf5e2493f40c66170730a2a82a550937
yoleoghub <164607154+yoleoghub@users.noreply.github.com>
2024-03-23 17:08:08 +0900

    Add files via upload

---
+++ java/VarEx2.java
@@ -0,0 +1,26 @@
+package variable.ex;
+
+public class VarEx2 {
+    /*
+    1.변수 num1을 선언하고, 이에 10을 할당하세요.
+    2.변수 num2를 선언하고, 이에 20을 할당하세요.
+    3.두 변수의 합을 구하고, 그 결과를 새로운 변수 sum 에 저장하세요.
+    4.sum 변수의 값을 출력하세요.
+     */
+
+    // 답 안 보고 직접 작성
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+
+    // 정답과 동일해서 따로 정답은 적지 않음. 다만 sout문장이 17행 아닌 16행에 있었음
+
+    // 느낀 점
+    /*
+    15행처럼 이미 13,14행에 변수의 데이터타입을 지정해줬다면, int num1 + int num2할 필요 없이 변수 명만 적어주면 된다는 걸 다시 한 번 더 느낌
+     */
+}
