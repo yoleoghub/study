1af557a0204e587ef1b072dd0f5287346a22f511
yoleoghub <164607154+yoleoghub@users.noreply.github.com>
2024-03-23 17:03:34 +0900

    Add files via upload

---
+++ java/VarEx1.java
@@ -0,0 +1,39 @@
+package variable.ex;
+
+public class VarEx1 {
+
+    // 다음 코드에 반복해서 나오는 숫자 10, 3을 다른 숫자로 한번에 변경할 수 있도록 다음을 변수 num1, num2 를 사용하도록 변경해보세요.
+    public static void main(String[] args) {
+        /*
+        System.out.println(10 + 3);
+        System.out.println(10 - 3);
+        System.out.println(10 * 3);
+         */
+
+        // 정답 안 보고 직접 풀이
+        int num1, num2;
+        num1 = 10;
+        num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+
+        // 정답
+        /*
+        int num1 = 4;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+         */
+
+        // 느낀 점
+        /*
+        1) 변수 선언, 변수 초기화를 복습할 겸 선언과 초기화를 각각 했는데 변수와 초기화를 한번에 하는 게 좀 더 낫다는 걸 느꼈다
+        2) 답을 안 보고 직접 생각해보면서 코드를 작성하니 확실히 익혔다는 생각이 들어서 만족스러웠다.
+        다만 이미 너무 익숙한 내용이어서 바로 나온 것 같아서, 내가 어렵다고 느끼는 부분도 이렇게 바로 나올 수 있도록 반복 훈련하는 게 중요하다고 느꼈다.
+®         */
+    }
+}
